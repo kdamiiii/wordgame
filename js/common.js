46b89fd2460f9ab3258519b4f21954e25b500cd9
kurtdamian020199@gmail.com <kurt.damian@phitopolis.com>
2024-08-17 23:31:14 +0800

    Pre-tested final commmit

--- js/common.js
+++ js/common.js
@@ -234,3 +234,263 @@ function setWordViewersToNull(words, name){
         return wordie
     })
 }
+
+function drawCards(playerData, turn, gameData, playerRole, words, visible, announcement, update, cardsContainer, db, hash, ref, get, playerName){
+    document.body.style.backgroundColor = turn == 
+    'blue' ? '#213268' : '#662727';
+    const opponent = turn == 'blue' ? 'red' : 'blue';
+    if(gameData.clue){
+        announcement.innerHTML = `${turn.toUpperCase()} TEAM is Guessing! CLUE: ${gameData.clue} [${gameData.clueCount}]`;
+        if(playerRole == turn){
+            for(let i in words){
+                const newDiv = createCard(words[i].name, words[i].color, visible, words[i].viewed);
+                
+                if(!words[i].viewed){
+                    const button = document.createElement('button');
+                    button.className = 'btn btn-success';
+                    button.innerText = "Select";
+                    
+                    button.onclick = function(){
+                        const action = {
+                            type:'select',
+                            player:playerName,
+                            word: words[i].name,
+                            wordColor: words[i].color,
+                            team: turn
+                        }
+                        let updates = null;
+                        if(words[i].color == turn){
+                            let clueCount = gameData.clueCount - 1;
+                            const nextTurn = clueCount < 1 ? opponent : turn;
+                            const turnScore = gameData[`${turn}Cards`] - 1;
+
+                            let clue = clueCount < 1? null : gameData.clue;
+
+                            updates = {
+                                [`lobbies/${hash}/gameStatus/clue/`]:clue,
+                                [`lobbies/${hash}/gameStatus/clueCount/`]:clueCount,
+                                [`lobbies/${hash}/gameStatus/turn/`]:nextTurn,
+                                [`lobbies/${hash}/gameStatus/${turn}Cards/`]:turnScore,
+                                [`lobbies/${hash}/gameStatus/words`]: setWordViewersToNull(words, words[i].name),
+                                [`lobbies/${hash}/gameStatus/logs`]: [...gameData.logs, action] 
+                            }
+
+                            if(turnScore == 0) {
+                                addPlayerScore(playerData, get, turn, update, ref, db);
+                            }
+                        }
+                        else if(words[i].color == 'neutral'){
+                            updates = {
+                                [`lobbies/${hash}/gameStatus/clue/`]:null,
+                                [`lobbies/${hash}/gameStatus/clueCount/`]:null,
+                                [`lobbies/${hash}/gameStatus/turn/`]:opponent,
+                                [`lobbies/${hash}/gameStatus/words`]: setWordViewersToNull(words, words[i].name),
+                                [`lobbies/${hash}/gameStatus/logs`]: [...gameData.logs, action] 
+                            }
+                        }
+                        else if(words[i].color == 'black'){
+                            updates = {
+                                [`lobbies/${hash}/gameStatus/clue/`]:null,
+                                [`lobbies/${hash}/gameStatus/clueCount/`]:null,
+                                [`lobbies/${hash}/gameStatus/turn/`]:opponent,
+                                [`lobbies/${hash}/gameStatus/${opponent}Cards/`]:0,
+                                [`lobbies/${hash}/gameStatus/words`]: setWordViewersToNull(words, words[i].name),
+                                [`lobbies/${hash}/gameStatus/logs`]: [...gameData.logs, action] 
+                            }
+
+                            addPlayerScore(playerData, get, opponent, update, ref, db);
+                        }
+                        else {
+                            const turnScore = gameData[`${opponent}Cards`] - 1;
+
+                            updates = {
+                                [`lobbies/${hash}/gameStatus/clue/`]:null,
+                                [`lobbies/${hash}/gameStatus/clueCount/`]:null,
+                                [`lobbies/${hash}/gameStatus/turn/`]:opponent,
+                                [`lobbies/${hash}/gameStatus/${opponent}Cards/`]:turnScore,
+                                [`lobbies/${hash}/gameStatus/words`]: setWordViewersToNull(words, words[i].name),
+                                [`lobbies/${hash}/gameStatus/logs`]: [...gameData.logs, action] 
+                            }
+
+                            if(turnScore == 0) {
+                                addPlayerScore(playerData, get, opponent, update, ref, db);
+                            }
+                        }
+                        update(ref(db), updates);
+                    }
+                    newDiv.appendChild(button);
+                }
+
+                newDiv.onclick = function(){
+                    if(!words[i].viewed){
+                        if(words[i].viewers && words[i]?.viewers.includes(playerName)){
+                            let updatedViewers = words[i].viewers.filter(item => item !== playerName);
+                            const updates = {
+                                [`lobbies/${hash}/gameStatus/words/${i}/viewers`] : updatedViewers
+                            }
+                            update(ref(db), updates);
+                        }
+                        else if(words[i].viewers == null){
+
+                            const updates = {
+                                [`lobbies/${hash}/gameStatus/words/${i}/viewers`] : [playerName]
+                            }
+                            update(ref(db), updates);
+                        }
+                        else{
+                            const updates = {
+                                [`lobbies/${hash}/gameStatus/words/${i}/viewers`] : [...words[i].viewers, playerName]
+                            }
+                            update(ref(db), updates);
+                        }
+                    }
+                }
+
+                if(!words[i].viewed){
+                    newDiv.appendChild(createUserHints(words[i].viewers, playerData));
+                }
+
+                cardsContainer.appendChild(newDiv);
+            }
+        }
+        else{
+            for(let i in words){
+                const newDiv = createCard(words[i].name, words[i].color, visible, words[i].viewed);
+                newDiv.appendChild(createUserHints(words[i].viewers, playerData));
+                cardsContainer.appendChild(newDiv);
+            }
+        }
+    }
+    else{
+        if(playerRole == `${turn}-spymaster`){
+            for(let i in words){
+                cardsContainer.appendChild(createCard(words[i].name, words[i].color, visible, words[i].viewed))
+            }
+
+            const container = document.createElement('div');
+            container.className = 'input-container';
+            // container.style.width = '40%';
+
+            const input = document.createElement('input');
+            input.type = 'text';
+            input.className = 'form-control-lg';
+            input.id = 'clue-input';
+            input.maxLength = 20;
+
+            const numberInput = document.createElement('input');
+            numberInput.id = 'number-input-dog'
+            numberInput.type = 'number';
+            numberInput.className = 'form-control-lg number-input';
+            numberInput.max=10;
+            numberInput.min=1;
+
+            // Append the outer div to the body (or another container element)
+            const labelll = document.createElement('h4');
+            labelll.innerText = 'CLUE:'
+            labelll.style.color = 'white'
+            container.appendChild(labelll);
+            container.appendChild(input);
+
+            const numberContainer = document.createElement('div');
+            numberContainer.className = 'input-container';
+            numberContainer.style.width = '10em !important';
+
+            numberContainer.appendChild(numberInput);
+
+            const button = document.createElement('button');
+            button.type = 'button';
+            button.className = 'btn btn-success';
+            button.innerText = 'Confirm'
+            button.style.height = '100%'
+
+            button.onclick = function(){
+                const num = document.getElementById('number-input-dog').value;
+                const textinput = document.getElementById('clue-input').value;
+                if(num && textinput){
+                    const action = {
+                        type:'clue',
+                        player:playerName,
+                        word: textinput,
+                        team: turn,
+                        wordCount: num
+                    }
+                    const updates = {
+                        [`lobbies/${hash}/gameStatus/clue`]:textinput,
+                        [`lobbies/${hash}/gameStatus/clueCount`]: num,
+                        [`lobbies/${hash}/gameStatus/logs`]: 
+                        gameData.logs ? [...gameData.logs, action] : [action] 
+                    }
+                    update(ref(db), updates);
+                }
+                else{
+                    alert('Missing inputs');
+                }
+            }
+
+            document.getElementById('player-options').appendChild(container);
+            document.getElementById('player-options').appendChild(numberContainer);
+            document.getElementById('player-options').appendChild(button);
+        }
+        else{
+            announcement.innerHTML = `${turn.toString() .toUpperCase()} SPYMASTER is giving a clue`
+            for(let i in words){
+                const newDiv = createCard(words[i].name, words[i].color, visible, words[i].viewed);
+                // newDiv.appendChild(createUserHints(words[i].viewers), playerData);
+                cardsContainer.appendChild(newDiv);
+            }
+        }
+    }
+}
+
+function showWinner(winnerColor, words, cardsContainer){
+    document.body.style.backgroundColor = winnerColor == 'blue' ? '#213268' : '#662727';
+    const winner = document.createElement('div');
+    winner.className = 'winner';
+    winner.innerText = `${winnerColor.toUpperCase()} TEAM WINS!`
+    announcement.appendChild(winner);
+    for(let i in words){
+        const newDiv = createCard(words[i].name, words[i].color, true, words[i].viewed);
+        cardsContainer.appendChild(newDiv);
+    }
+    const endButton = document.getElementById('leaveMatch');
+    endButton.style.display = 'flex';
+
+    endButton.onclick = function(){
+        localStorage.removeItem('lobbyHash');
+        update(ref(db), {
+            ['lobbies/'+hash+'/players']: playerData.filter((player)=>player!==playerName)
+        })
+        const baseUrl = window.location.href.replace('game-start.html','lobbies.html');
+        const newUrl = `${baseUrl}`;
+        window.location.href = newUrl;
+    }
+}
+
+
+function addPlayerScore(playerData, get, winner, update, ref,db) {
+    let updates = {}
+    const accountsRef = ref(db, 'accounts');
+    get(accountsRef).then((accountsSnapshot)=>{
+        const accounts = accountsSnapshot.val();
+        for(let player in playerData) {
+            if(playerData[player].includes(winner)){
+                if(playerData[player].includes('spymaster')){
+                    updates[`accounts/${player}/winsAsSpy`] = accounts[player].winsAsSpy + 1;
+                }
+                else {
+                    updates[`accounts/${player}/winsAsPlayer`] = accounts[player].winsAsPlayer + 1;
+                }
+            }
+            else{
+                if(playerData[player].includes('spymaster')){
+                    updates[`accounts/${player}/losesAsSpy`] = accounts[player].losesAsSpy + 1;
+                }
+                else {
+                    updates[`accounts/${player}/losesAsPlayer`] = accounts[player].losesAsPlayer + 1;
+                }
+            }
+        }
+        
+        update(ref(db), updates);
+    })
+}
